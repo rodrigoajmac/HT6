05d95b1c41dc459e6c7518e37e02ee7f94939626
RodrigoAar <ajm22279@uvg.edu.gt>
2025-03-16 22:35:38 -0600

    Create IOperation.java
    Interfaz con los métodos para todas las estructuras

---
+++ src/main/java/org/example/IOperation.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface IOperation {
+    String GuardarPokemon(Pokemon pokemon);
+    String BuscarPokemon(String llave);
+    String MostrarPokemonTipo(String tipo);
+    String MostrarColeccion();
+    String MostrarPokemonHabilidad(String habilidad);
+}
