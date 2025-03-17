fffa502823d95eabe9e3ed71638644440901c67b
RodrigoAar <ajm22279@uvg.edu.gt>
2025-03-17 09:28:37 -0600

    Update MapPokemon.java
    Se eliminaron los atributos y métodos de la clase dado que no se utilizaban

--- src/main/java/org/example/MapPokemon.java
+++ src/main/java/org/example/MapPokemon.java
@@ -1,8 +1,4 @@
 package org.example;
 
 abstract class MapPokemon implements IOperation{
-    private String usuario;
-
-    public String getUsuario() { return usuario; }
-    public void setUsuario(String usuario) { this.usuario = usuario; }
 }
